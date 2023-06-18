73bab2149f027b5f0db5a6251b131b4738527a4d
Chad Coates <chroco@gmail.com>
2023-06-18 15:44:19 -0700

    Almost done!

--- DecodeMessageUpdated.c
+++ DecodeMessageUpdated.c
@@ -12,8 +12,6 @@
 // Special characters for this exercise: EF is a line ending, FF ends the message.
 
 #include "stdio.h"
-#include "ctype.h"
-#include "inttypes.h"
 
 char const * pMessage = {
     "436F6E67726174756C6174696F6E732120596F75"
@@ -26,117 +24,72 @@ char const * pMessage = {
     "206F7665723F20436865636B21EF"
 };
 
-// DecodeAndPrint() decodes a single message
-int OldDecodeAndPrint(char const *pMessage)
+char myToUpper(const char c)
 {
-  int i;
-  int numberOfLines = 0;
-    
-  if(pMessage == NULL)
+  if (c >= 'a' && c <= 'z')
   {
-    printf("\nError, no message!");
-    
-    return 0; // returns 0 because no lines are parsed
+    return c - 'a' + 'A';
   }
 
-  printf("\nMessage:\n%s\n", pMessage);
- 
-  printf("\nParsed message:\n");
-  
-  for(i = 0; pMessage[i]; ++i) // Walk through the message.
-  {
-    if(!isxdigit(pMessage[i])) // checks to verify the char is a hexidecimal digit
-    {
-      printf("\nError, %c not recognized as a hexidecimal number", pMessage[i]);
-  
-      return numberOfLines;
-    }
-        
-    printf("%c", pMessage[i]); // print each character to stdout
-    
-    if(i % 2 == 1) // check characters 2 at a time
-    {
-      if(toupper(pMessage[i - 1]) == 'E' && toupper(pMessage[i]) == 'F')
-      {
-        ++numberOfLines;
-        
-        printf(" \\n\n"); // print new lines
-      }
-
-      if(toupper(pMessage[i - 1]) == 'F' && toupper(pMessage[i]) == 'F')
-      {
-        return numberOfLines; // stop printing the message and return
-      }
-    }
-  }
-
-  printf("\nEnd of characters, no end of message (FF) received!\n"); 
-
-  return numberOfLines;  
+  return c;
 }
 
-char asciiHexToAscii(const char *pmessage, const int index)
+int isHexDigit(const char c)
 {
-//  int i = 0;
-  uint8_t test = 0x0;
-  uint8_t base = (1 << 4);
-
-  char hex[3] = { pmessage[index * 2], pmessage[index * 2 + 1], '\0' };
-  test = (char)((uint8_t)(hex[0] - '0') * base  + (uint8_t)(hex[1] - '0')); 
-  //printf("%c", (char)test); 
-/*
-  printf("\n");
-  printf("ascii: %c, %c\n", hex[0], hex[1]); 
-  printf("hex:  0x%02x. 0x%02x\n", hex[0], hex[1]); 
-  printf("uns:  %u. %u\n", hex[0], hex[1]); 
-  printf("hex : %x\n", test); 
-  printf("ascii : %c\n", (char)test); 
-//*/
-/*
-  for (i = 0; i < 2; ++i)
+  if((c >= '0' && c <= '9') || (c  >= 'A' && c <= 'F'))
   {
-    char hex[3] = { pmessage[i * 2], pmessage[i * 2 + 1], '\0' };
-    test = (hex[0] << 4) | hex[1]; 
-  }
-//*/
-  return test;
-}
-
-int toAsciiStdOut(uint8_t val)
-{
-  if(sizeof(char) != sizeof(uint8_t))
-  {
-    printf("type size mismatch error!\n");
     return 1;
   }
 
-  const char highchar = '4';
-  const char lowchar = '1';
-  uint8_t h4bits = 0x0;
-  uint8_t l4bits = 0x0;
-
-  printf("\n\n");
-  printf("char     : %c\n", val);
-  printf("uint8_t  : %04u\n", val);
-  printf("hex      : %04x\n", val);
-  
   return 0;
 }
 
+char hexToAscii(const char c)
+{
+  char temp = myToUpper(c);
+  
+  if(isHexDigit(temp))
+  {
+    if(temp >= '0' && temp <= '9')
+    {
+      return temp - '0';
+    }
+    else if (temp >= 'A' && temp <= 'Z')
+    {
+      return temp - 'A' + 10;
+    }
+  }
+
+  printf("Error!\n");
+
+  return -1;
+}
+
+char asciiHexToAscii(const char *pMessage, const int index)
+{
+  if(!pMessage)
+  {
+    return -1;
+  }
+
+  char hex[2] = { pMessage[index], pMessage[index +  1] };
+  hex[0] = hexToAscii(hex[0]);
+  hex[1] = hexToAscii(hex[1]);
+
+  return (hex[0] << 4) + hex[1]; 
+}
+
 // DecodeAndPrint() decodes a single message
-int DecodeAndPrint(char const *pMessage)
+int DecodeAndPrint(const char *pMessage)
 {
   /*
   * Walk through the message.
   * print each character, print new lines, and stop printing the message
   * at the end of the message text, according to the special characters.
   */
+
   int i = 0;
   int numberOfLines = 0;
-  
-  //printf("\nMessage:\n%s\n", pMessage);
- 
-  printf("\n\nParsed message:\n");
   
   if(pMessage == NULL)
   {
@@ -149,23 +102,21 @@ int DecodeAndPrint(char const *pMessage)
   {
     if(i % 2 == 1) // check characters 2 at a time
     {
-      printf("%c", asciiHexToAscii(pMessage, i - 1)); // print each character to stdout
-     /* 
-      if(toupper(pMessage[i - 1]) == 'E' && toupper(pMessage[i]) == 'F')
+      if(pMessage[i - 1] == 'E' && pMessage[i] == 'F')
       {
         ++numberOfLines;
-        printf(" \\n\n"); // print new lines
+        printf("\n"); // print new lines
       }
-      else if(toupper(pMessage[i - 1]) == 'F' && toupper(pMessage[i]) == 'F')
+      else if(pMessage[i - 1] == 'F' && pMessage[i] == 'F')
       {
         return numberOfLines; // stop printing the message and return
       }
-      */
+      else
+      {
+        printf("%c", asciiHexToAscii(pMessage, i - 1));// print each character to stdout
+      }
     }
   }
-
-
-  //toAsciiStdOut('A');
 
   /* Correct this code to return the number of lines printed from the message */
   return numberOfLines;
@@ -173,11 +124,11 @@ int DecodeAndPrint(char const *pMessage)
 
 int main()
 {
-  int qty;
-  qty = OldDecodeAndPrint(pMessage);
+  int qty = 0;
+  
+  printf("\nMessage:\n%s\n", pMessage);
+  printf("\nParsed message:\n");
   qty = DecodeAndPrint(pMessage);
-//  asciiHexToAscii(pMessage, 0);
-
   printf("\n\nNumber of lines that were parsed: %d\r\n", qty);
 
   return 0;
